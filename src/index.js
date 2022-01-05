c42d1d8a11ea39fe79275427ed004404c92c8f39
sokolw <sokolw21@gmail.com>
2022-01-05 14:45:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let result = "";
+    let i = 0
+    n = `${n}`;
+    while (i < n.length){
+        result = `${n[i]}${result}`;
+        i++;
+    }
+    return parseInt(result);
 }
+
